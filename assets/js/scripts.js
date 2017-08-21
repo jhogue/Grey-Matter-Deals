1743d2acbfd0fac1e76a2360832d6cf84fd3cc80
jhogue <highchairdesignhaus@gmail.com>
2017-08-21 15:55:12 -0400

    Add Waypoints and initialize

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
   
-  //Clone the Main nav to create the mobile drawer
+  // Clone the Main nav to create the mobile drawer
   $('#b365-mainnav').clone().prop('id', 'b365-mainnav__js-clone' ).addClass('drawer__nav').appendTo('.drawer');
   
   // Javascript to toggle the mobile menus on Primary Source & .com
@@ -18,12 +18,30 @@ $(document).ready(function() {
   });
   
   
+  // Deals page
+  // Clone the Redeem actions for an adhesive bottom bar
+  $('#deal-redeem').clone().prop('id', 'deal-redeem__js-clone' ).addClass('deal__redeem deal__redeem--adhesive').appendTo('.deal__details');
+  $('#deal-brand').clone().prop('id', 'deal-actions__js-clone' ).addClass('deal__title__brand').prependTo('#deal-redeem__js-clone');
+  
+  // Waypoints for the redeem options
+  var waypoint = new Waypoint({
+    element: document.getElementById('deal-wrapper'),
+    handler: function(direction) {
+      // Action
+      //console.log('#deal-wrapper has hit the top of the viewport');
+      $('body').toggleClass('js-adhesive-redeem');
+      $('#deal-redeem__js-clone').toggleClass('deal__redeem--visible');
+    },
+    offset: 0
+  });
+
+
   /* 
    * Animate some scrolling for smoother transitions 
    * http://css-tricks.com/snippets/jquery/smooth-scrolling/
    */
   $(function() {
-    $('.smooth-scroll').click(function(e) {
+    $('.js-smooth-scroll').click(function(e) {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
